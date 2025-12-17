a7236a540b65c974195661b97ace27ac180f5c18
alvianzf <alvianzf@gmail.com>
2025-12-17 16:12:43 +0700

    feat: add try-catch for prisma service

--- src/prisma/prisma.service.ts
+++ src/prisma/prisma.service.ts
@@ -4,6 +4,10 @@ import { PrismaClient } from '@prisma/client';
 @Injectable()
 export class PrismaService extends PrismaClient implements OnModuleInit {
   async onModuleInit() {
-    await this.$connect();
+    try {
+      await this.$connect();
+    } catch (err) {
+      console.error('Prisma failed to connect', err);
+    }
   }
 }
